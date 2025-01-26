38632927c4faf052eca1d6ee33cd6b82c7cbf53f
vltruong01 <157948765+vltruong01@users.noreply.github.com>
2025-01-26 14:53:11 +0700

    final
    final

--- task3/create_vocabpkl.py
+++ task3/create_vocabpkl.py
@@ -1,7 +1,7 @@
 import pickle
 
 # Load the dataset with UTF-8 encoding
-with open('shakespeare.txt', 'r', encoding='utf-8') as file:
+with open('../shakespeare.txt', 'r', encoding='utf-8') as file:
     text = file.read()
 
 # Tokenization
@@ -11,5 +11,5 @@ tokens = text.split()
 vocab = sorted(set(tokens))
 
 # Save the vocabulary to a pickle file
-with open('task3/vocab.pkl', 'wb') as f:
+with open('vocab.pkl', 'wb') as f:
     pickle.dump(vocab, f)
